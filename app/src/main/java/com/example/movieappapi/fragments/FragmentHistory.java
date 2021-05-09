d8aebbaf62817544c51d7b1c71ef14a8bdf70a5b
indiraseptianita <indiraseptianita269@gmail.com>
2021-05-09 22:24:27 +0700

    Initial commit

---
+++ app/src/main/java/com/example/movieappapi/fragments/FragmentHistory.java
@@ -0,0 +1,35 @@
+package com.example.movieappapi.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.movieappapi.R;
+
+public class FragmentHistory extends Fragment {
+
+    public FragmentHistory(){
+
+    }
+    public static FragmentHistory newInstance(){
+        FragmentHistory fragment = new FragmentHistory();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.movie_history, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/movieappapi/networks/Consts.java
@@ -0,0 +1,12 @@
+package com.example.movieappapi.networks;
+
+public class Consts {
+
+    public static final String BASEURL = "https://api.themoviedb.org/3/";
+    public static final String APIKEY = "92e614bd264d164515d45076380f67d5";
+    public static final String LANGUAGE = "en-US";
+    public static final String PAGE = "1";
+    public static final String IMAGEBASEURL = "https://image.tmdb.org/t/p/w500/";
+    public static final String APIERROR = "DEBUG:API ERROR";
+
+}
